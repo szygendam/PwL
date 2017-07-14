bdfd47f5592d04e56f05ea9f2eb7f84f4050d80a
Marek Szygenda <marek.szygenda@gmail.com>
2017-07-14 14:55:19 +0200

    temp version of getLastLocation() createLocationRequest() getCurrentLocationSettings() have been added

--- PWL/app/src/main/java/com/example/szygendm/pwl/MapsActivity.java
+++ PWL/app/src/main/java/com/example/szygendm/pwl/MapsActivity.java
@@ -1,18 +1,36 @@
 package com.example.szygendm.pwl;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.content.IntentSender;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.annotation.*;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.*;
+import com.google.android.gms.common.api.*;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private final String _PACKAGE = "com.example.szygendm.pwl";
+    protected static final int _REQUEST_CHECK_SETTINGS = 0x1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
 
@@ -41,6 +61,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in home and move the camera
         LatLng dom = new LatLng(52.105469, 21.039901);
         mMap.addMarker(new MarkerOptions().position(dom).title("Dom"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(dom));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dom,(float)16.0));
+        getLastLocation();
+        createLocationRequest();
+    }
+
+    public void getLastLocation(){
+        PackageManager packageManager = getApplicationContext().getPackageManager();
+        if (packageManager.checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, _PACKAGE) == PackageManager.PERMISSION_GRANTED ) { //TODO
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                LatLng lastKnowLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(lastKnowLocation).title("Last Location"));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(lastKnowLocation));
+                            }
+                        }
+                    });
+        }
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        getCurrentLocationSettings(mLocationRequest);
+    }
+
+    public void getCurrentLocationSettings(LocationRequest mLocationRequest){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        //Prompt user to change location settings
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(MainActivity.this, //TODO
+                                    _REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
     }
 }
